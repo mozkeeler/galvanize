bf6f7f66e17fa8df83e7f9ca70cd6cbfa1c90529
Dana Keeler <mozkeeler@gmail.com>
2019-04-26 15:06:04 -0700

    update to ring 0.14.6

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,10 @@ use ring::aead;
 use ring::digest;
 use ring::pbkdf2;
 use ring::rand;
+use ring::rand::SecureRandom;
 use std::env;
 use std::io::prelude::*;
+use std::num::NonZeroU32;
 use std::process;
 
 // Adapted from https://doc.rust-lang.org/getopts/getopts/index.html
@@ -19,7 +21,8 @@ fn print_usage(program: &str, opts: Options) {
 
 fn derive_key(password: &str, key_out: &mut [u8]) {
     let salt = vec![0u8; 0];
-    pbkdf2::derive(&pbkdf2::HMAC_SHA256, 1000, &salt, password.as_bytes(), key_out);
+    pbkdf2::derive(&digest::SHA256, NonZeroU32::new(1000).unwrap(), &salt, password.as_bytes(),
+                   key_out);
 }
 
 fn read_stdin(buffer: &mut Vec<u8>) {
@@ -39,17 +42,17 @@ const NONCE_LEN: usize = 96 / 8;
 fn encrypt(key: &[u8]) {
     let mut data = Vec::new();
     read_stdin(&mut data);
-    for _ in 0..aead::MAX_OVERHEAD_LEN {
+    for _ in 0..aead::MAX_TAG_LEN {
         data.push(0);
     }
-    let mut nonce = vec![0u8; NONCE_LEN];
+    let mut nonce = [0u8; aead::NONCE_LEN];
     let rng = rand::SystemRandom::new();
     assert!(rng.fill(&mut nonce).is_ok());
     write_bytes(&nonce);
+    let nonce = aead::Nonce::assume_unique_for_key(nonce);
     let seal_key = aead::SealingKey::new(&aead::AES_256_GCM, key).unwrap();
-    let ad = vec![0u8; 0];
-    let out_len = match aead::seal_in_place(&seal_key, &nonce, &mut data, aead::MAX_OVERHEAD_LEN,
-                                            &ad) {
+    let out_len = match aead::seal_in_place(&seal_key, nonce, aead::Aad::empty(), &mut data,
+                                            aead::MAX_TAG_LEN) {
         Ok(out_len) => out_len,
         Err(e) => {
             let mut stderr = std::io::stderr();
@@ -63,21 +66,22 @@ fn encrypt(key: &[u8]) {
 fn decrypt(key: &[u8]) {
     let mut data = Vec::new();
     read_stdin(&mut data);
-    let mut nonce = Vec::new();
+    let mut nonce = [0u8; aead::NONCE_LEN];
     for i in 0..NONCE_LEN {
-      nonce.push(data[i]);
+      nonce[i] = data[i];
     }
+    let nonce = aead::Nonce::assume_unique_for_key(nonce);
     let open_key = aead::OpeningKey::new(&aead::AES_256_GCM, key).unwrap();
-    let ad = vec![0u8; 0];
-    let out_len = match aead::open_in_place(&open_key, &nonce, NONCE_LEN, &mut data, &ad) {
-        Ok(out_len) => out_len,
+    let out = match aead::open_in_place(&open_key, nonce, aead::Aad::empty(), NONCE_LEN,
+                                        &mut data) {
+        Ok(out) => out,
         Err(e) => {
             let mut stderr = std::io::stderr();
             writeln!(&mut stderr, "{}", e.to_string()).unwrap();
             process::exit(1);
         }
     };
-    write_bytes(&data[..out_len]);
+    write_bytes(out);
 }
 
 fn main() {
