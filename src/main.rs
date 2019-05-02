925425eea1e9b8d6c4176ae054fd1eb431b13cfe
Dana Keeler <mozkeeler@gmail.com>
2019-05-02 16:45:43 -0700

    prompt for password, basic loop

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 extern crate ring;
+extern crate rpassword;
 
 use ring::rand::SecureRandom;
 use ring::{aead, digest, error, pbkdf2, rand};
+use std::env;
+use std::fs::File;
+use std::io::{stdin, Error, ErrorKind, Read};
 use std::num::NonZeroU32;
 
 fn encrypt(key: &[u8], plaintext: &[u8]) -> Result<Vec<u8>, error::Unspecified> {
@@ -60,8 +64,77 @@ fn password_to_key(password: &str) -> Result<[u8; digest::SHA256_OUTPUT_LEN], er
     Ok(key)
 }
 
-fn main() {
-    println!("hello, world");
+fn read_file(filename: &str) -> Result<Vec<u8>, Error> {
+    let mut file = match File::open(filename) {
+        Ok(file) => file,
+        Err(_) => {
+            File::create(filename)?;
+            File::open(filename)?
+        }
+    };
+    let mut contents = Vec::with_capacity(file.metadata()?.len() as usize);
+    file.read_to_end(&mut contents)?;
+    Ok(contents)
+}
+
+enum Action {
+    List,
+    New,
+    Quit,
+    Unknown(String),
+}
+
+impl Action {
+    fn from_description(description: String) -> Action {
+        if description.eq_ignore_ascii_case("L") {
+            Action::List
+        } else if description.eq_ignore_ascii_case("N") {
+            Action::New
+        } else if description.eq_ignore_ascii_case("Q") {
+            Action::Quit
+        } else {
+            Action::Unknown(description)
+        }
+    }
+}
+
+fn prompt() -> Result<Action, Error> {
+    println!("(L)ist (N)ew (Q)uit?");
+    let mut input = String::new();
+    let _ = stdin().read_line(&mut input)?;
+    input
+        .pop()
+        .ok_or(Error::new(ErrorKind::Other, "no newline?"))?; // remove newline
+    Ok(Action::from_description(input))
+}
+
+fn main() -> Result<(), Error> {
+    let db_filename = env::args()
+        .nth(1)
+        .ok_or(Error::new(ErrorKind::Other, "expected db filename"))?;
+    let db_contents = read_file(&db_filename)?;
+    let password = rpassword::read_password_from_tty(Some("password: "))?;
+    let key = password_to_key(&password).map_err(|e| Error::new(ErrorKind::Other, e))?;
+    let mut entries = Vec::new();
+    if !db_contents.is_empty() {
+        let decrypted = decrypt(&key, &db_contents).map_err(|e| Error::new(ErrorKind::Other, e))?;
+        let as_utf8 = String::from_utf8(decrypted).map_err(|e| Error::new(ErrorKind::Other, e))?;
+        for line in as_utf8.lines() {
+            entries.push(line.to_owned());
+        }
+    }
+    loop {
+        match prompt()? {
+            Action::List => println!("listing..."),
+            Action::New => println!("new..."),
+            Action::Quit => {
+                println!("quitting...");
+                break;
+            }
+            Action::Unknown(value) => println!("Unknown: {}", value),
+        }
+    }
+    Ok(())
 }
 
 #[cfg(test)]
